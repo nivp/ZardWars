bc3a0ed59fde9938b114fadf7cc8a913a716d40c
nivp <nivp@campus.technion.ac.il>
2019-06-27 12:34:06 +0300

    Update config.php

--- Upload/config.php
+++ Upload/config.php
@@ -1,8 +1,8 @@
 <?php
-$dhost = "sql310.epizy.com"; # Usually localhost or the IP address of the host
-$dusername = "epiz_23979745"; # Database username
-$dpassword = "nDQUSvFU"; # Database password
-$ddatabase = "epiz_23979745_zardgoggery"; # Database name
+$dhost = "localhost"; # Usually localhost or the IP address of the host
+$dusername = "username"; # Database username
+$dpassword = "password"; # Database password
+$ddatabase = "database_name"; # Database name
 
 $con = mysql_connect($dhost, $dusername, $dpassword) or die ("Cannot connect to the database"); 
 mysql_select_db($ddatabase, $con) or die ("Cannot select the table");
